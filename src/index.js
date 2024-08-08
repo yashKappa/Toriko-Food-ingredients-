be3aef347394f3ad666ec39426a04ed96552dd26
yash <yashsaundalkar0@gmail.com>
2024-08-08 20:53:14 +0530

    ggd

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import App from './App';
 import './index.css';
+import Products from './components/Products';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Router basename="/Toriko-Food-ingredients-">
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/Products" element={<Products />} /> 
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
